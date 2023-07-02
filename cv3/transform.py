b2fec418549c9c29edfd2f34d81b568177a765b8
medoedka <nick99043@gmail.com>
2023-07-02 13:20:01 +0300

    Refactored some transform.py logic

--- cv3/transform.py
+++ cv3/transform.py
@@ -2,85 +2,111 @@ import cv2
 import numpy as np
 from functools import partial
 import warnings
+from nptyping import Int, NDArray, Shape
+from typing import Tuple, Optional, Union
+from ._utils import COLORS_RGB_DICT
+
 
 from ._utils import type_decorator, _relative_check, _relative_handle, _process_color, _handle_rect_coords
 from .utils import xywh2xyxy, ccwh2xyxy, yyxx2xyxy
 
 __all__ = [
-    'vflip', 'hflip', 'dflip',
+    'vflip',
+    'hflip',
+    'dflip',
     'transform',
-    'rotate', 'rotate90', 'rotate180', 'rotate270',
+    'rotate',
+    'rotate90',
+    'rotate180',
+    'rotate270',
     'scale',
-    'shift', 'translate',
-    'xshift', 'xtranslate',
-    'yshift', 'ytranslate',
+    'shift',
+    'translate',
+    'xshift',
+    'xtranslate',
+    'yshift',
+    'ytranslate',
     'resize',
     'crop',
-    'pad', 'copyMakeBorder',
+    'pad',
+    'copyMakeBorder'
 ]
 
-_INTER_DICT = {
-    'nearest': cv2.INTER_NEAREST,
-    'linear': cv2.INTER_LINEAR,
-    'area': cv2.INTER_AREA,
-    'cubic': cv2.INTER_CUBIC,
-    'lanczos4': cv2.INTER_LANCZOS4
-}
+
+def _inter_flag_match(flag: str):
+    inter_mapping = {
+        'nearest': cv2.INTER_NEAREST,
+        'linear': cv2.INTER_LINEAR,
+        'area': cv2.INTER_AREA,
+        'cubic': cv2.INTER_CUBIC,
+        'lanczos4': cv2.INTER_LANCZOS4
+    }
+    if flag not in inter_mapping.keys() or not isinstance(flag, str):
+        raise ValueError(f'Parameter "inter" has to be a string from: {list(inter_mapping.keys())}')
+    return inter_mapping[flag]
 
 
-def _inter_flag_match(flag):
-    assert flag in _INTER_DICT, f'no such flag: "{flag}". Available: {", ".join(_INTER_DICT.keys())}'
-    return _INTER_DICT[flag]
+def _border_flag_match(flag: str):
+    border_mapping = {
+        'constant': cv2.BORDER_CONSTANT,
+        'replicate': cv2.BORDER_REPLICATE,
+        'reflect': cv2.BORDER_REFLECT,
+        'wrap': cv2.BORDER_WRAP,
+        'default': cv2.BORDER_DEFAULT
+    }
+    if flag not in border_mapping.keys() or not isinstance(flag, str):
+        raise ValueError(f'Parameter `border` has to be a string from: {list(border_mapping.keys())}')
+    return border_mapping[flag]
 
 
-_BORDER_DICT = {
-    'constant': cv2.BORDER_CONSTANT,
-    'replicate': cv2.BORDER_REPLICATE,
-    'reflect': cv2.BORDER_REFLECT,
-    'wrap': cv2.BORDER_WRAP,
-    'default': cv2.BORDER_DEFAULT,
-    # 'transparent': cv2.BORDER_TRANSPARENT
-}
+def _color_value_check(color: Optional[Union[int, float, str, Tuple[int, int, int]]]) -> Optional[Union[int, float, Tuple[int, int, int]]]:
+    if isinstance(color, str):
+        colors_tuple = COLORS_RGB_DICT.get(color)
+        return colors_tuple
+    return color
 
 
-def _border_flag_match(flag):
-    assert flag in _BORDER_DICT, f'no such flag: "{flag}". Available: {", ".join(_BORDER_DICT.keys())}'
-    return _BORDER_DICT[flag]
+def vflip(image: NDArray):
+    return cv2.flip(src=image, flipCode=0)
 
 
-def _border_value_check(border, value):
-    if isinstance(border, str):
-        border = _border_flag_match(border)
-    if value is not None:
-        value = _process_color(value)
-        if border != cv2.BORDER_CONSTANT:
-            warnings.warn('`value` parameter is not used when `border` is not cv2.BORDER_CONSTANT')
-    return border, value
+def hflip(image: NDArray):
+    return cv2.flip(src=image, flipCode=1)
 
 
-@type_decorator
-def vflip(img):
-    return cv2.flip(img, 0)
+def dflip(image: NDArray):
+    return cv2.flip(src=image, flipCode=-1)
 
 
-@type_decorator
-def hflip(img):
-    return cv2.flip(img, 1)
-
-
-# diagonal flip
-@type_decorator
-def dflip(img):
-    return cv2.flip(img, -1)
-
-
-@type_decorator
-def transform(img, angle, scale, inter=cv2.INTER_LINEAR, border=cv2.BORDER_CONSTANT, value=None):
-    if isinstance(inter, str):
-        inter = _inter_flag_match(inter)
-    border, value = _border_value_check(border, value)
-    rot_mat = cv2.getRotationMatrix2D((img.shape[1] / 2, img.shape[0] / 2), angle, scale)
-    result = cv2.warpAffine(img, rot_mat, img.shape[1::-1], flags=inter, borderMode=border, borderValue=value)
+def transform(image: NDArray,
+              angle: float,
+              scale: float,
+              inter: str = 'linear',
+              border: str = 'constant',
+              value: Optional[Union[int, float, str, Tuple[int, int, int]]] = None) -> NDArray:
+    if not isinstance(value, (str, tuple)):
+        warnings.warn(
+            'Parameter `value` better use as a string, representing the color\
+             or tuple with 3 values, representng RGB channels values'
+        )
+    inter_mapped = _inter_flag_match(inter)
+    border_mapped = _border_flag_match(border)
+    valid_value = _color_value_check(value)
+    if border != cv2.BORDER_CONSTANT and valid_value:
+        warnings.warn('Parameter `value` is not used when `border` is not constant')
+    rot_matrix = cv2.getRotationMatrix2D(
+        center=(image.shape[1] / 2, image.shape[0] / 2),
+        angle=angle,
+        scale=scale
+    )
+    result = cv2.warpAffine(
+        src=image,
+        M=rot_matrix,
+        dsize=image.shape[1::-1],
+        flags=inter_mapped,
+        borderMode=border_mapped,
+        borderValue=valid_value
+    )
     return result
 
 
